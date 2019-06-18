7736622c1c6bd35e870ad906b4e83aeb0a1eec7d
Trzosik <a.trzoss@gmail.com>
2019-06-18 20:00:46 +0200

    black1

--- BlackJack_game.py
+++ BlackJack_game.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Card:
 
     ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"]
@@ -21,7 +24,7 @@ class Hand:
         if self.cards:
             rep = ''
             for card in self.cards:
-                rep += str(card) + ''
+                rep += str(card) + ' '
         else:
             rep = "<pusta>"
         return rep
@@ -32,4 +35,40 @@ class Hand:
     def add(self, card):
         self.cards.append(card)
 
+    def give(self, card, other_hand):
+        self.cards.remove(card)
+        other_hand.add(card)
+
+
+class Deck(Hand):
+
+    def populate(self):
+        for suit in Card.suits:
+            for rank in Card.ranks:
+                self.add(Card(rank, suit))
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def deal(self, hands, per_hand=1):
+        for rounds in range(per_hand):
+            for hand in hands:
+                if self.cards:
+                    top_card = self.cards[0]
+                    self.give(top_card, hand)
+                else:
+                    print('Nie mam wiecej')
+
+
+class Unprintable_Card(Card):
+
+    def __str__(self):
+        return "<utajona>"
+
+
+class Positionable_Card(Card):
+
+    def __init__(self, rank, suit, face_up=True):
+        super(Positionable_Card, self).__init__(rank, suit)
+        self.is_face_up = face_up
 
